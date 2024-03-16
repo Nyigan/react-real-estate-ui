53e91e36d7c2020cf6f7fccd78e32f68a2afedd1
Towett <towettf@yahoo.com>
2024-03-15 20:33:53 -0500

    section design

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,8 +1,40 @@
+import SearchBar from "../../components/searchBar/searchBar";
 import "./homePage.scss";
 
 function HomePage () {
   return (
-    <div className='homePage'>HomePage</div>
+    <div className="homePage">
+        <div className="textContainer">
+            <div className="wrapper">
+            <h1 className="title">
+            Find Real Estate & Your Dream Place
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+
+            <SearchBar />
+
+            <div className="boxes">
+                <div className="box">
+                    <h1>15+</h1>
+                    <h2>Years of Expertise</h2>
+                </div>
+                      <div className="box">
+                          <h1>200</h1>
+                          <h2>Excellence Awards</h2>
+                      </div>
+                      <div className="box">
+                          <h1>1500+</h1>
+                          <h2>Ready Property</h2>
+                      </div>
+            </div>
+        </div>
+        </div>
+        <div className="imgContainer">
+            <img src="/bg.png" alt="bg image" />
+        </div>
+    </div>
   )
 }
 
